7cc1a1045d5e2b9e1b559c074e801a9897d77ca1
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-12-20 17:23:39 +0900

    serve css file

---
+++ assets/src/assets/assets.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main(){
+  fs := http.FileServer(http.Dir("static/"))
+  http.Handle("/static/",http.StripPrefix("/static/",fs))
+  
+  http.ListenAndServe(":8000", nil)
+}
