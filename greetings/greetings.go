08aa49355fdd2e19ceec2ae9c0c6618a183765e9
hyuraku <32809703+hyuraku@users.noreply.github.com>
2021-03-08 22:38:03 +0900

    error handling

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,9 +1,15 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
-func Hello(name string) string {
- message := fmt.Sprintf("Hi, %v. Welcome!",name)
- return message
+func Hello(name string) (string, error) {
+	if name == ""{
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf("Hi, %v. Welcome!",name)
+	return message, nil
 }
 
--- hello2/hello2.go
+++ hello2/hello2.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Tom")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
